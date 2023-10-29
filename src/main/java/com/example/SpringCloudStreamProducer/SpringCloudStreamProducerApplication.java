44f9fbaf3608149c80649c20924766fd9acfcde2
Abhik <abhikgh.work@gmail.com>
2023-10-29 11:58:01 +0530

    Initial commit

---
+++ src/main/java/com/example/SpringCloudStreamProducer/SpringCloudStreamProducerApplication.java
@@ -0,0 +1,42 @@
+package com.example.SpringCloudStreamProducer;
+
+import com.example.SpringCloudStreamProducer.model.Book;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.annotation.StreamListener;
+import org.springframework.cloud.stream.messaging.Source;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.support.MessageBuilder;
+
+@SpringBootApplication
+@EnableBinding(Source.class)
+public class SpringCloudStreamProducerApplication implements CommandLineRunner {
+
+    @Autowired
+    private MessageChannel output;
+
+   // @Autowired
+    //@Qualifier(value = "input")
+    //private MessageChannel input;
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudStreamProducerApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        Book book = new Book(1, "attwn");
+        output.send(MessageBuilder.withPayload(book).build());
+        System.out.println("Book sent successfully");
+    }
+
+   /* @StreamListener(value = "input")
+    public void consumeUpdatedMessage(@Payload Book book){
+        System.out.println("Updated book :: " + book.getBookName());
+    }*/
+}
---
+++ src/test/java/com/example/SpringCloudStreamProducer/SpringCloudStreamProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringCloudStreamProducer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudStreamProducerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
